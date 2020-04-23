39d910d61addf05e2b64a87139e2e069f87c5e96
Victoria <Bernard.victoria22@gmail.com>
2020-04-23 16:50:48 -0500

    About for gateway uses custom component

---
+++ gateway-docs/src/pages/gateway/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { About } from 'cloudflare-docs-theme/src/components/About'
+export default () => (
+  <About
+    title="Cloudflare Gateway"
+    header="Secure your users, devices and network using Cloudflare Gateway"
+    about="Cloudflare Gateway protects enterprises, their devices, their networks and their data by securing every connection without compromising performance."
+    getStartedPath="/gateway/about"
+  ></About>
+)
